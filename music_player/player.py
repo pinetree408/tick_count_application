a1b08d3516eab8f6938a24d66bf3fbb926d5aeea
leesangyoon <pinetree408@gmail.com>
2019-05-20 14:55:51 +0900

    Add user interface at music player

--- music_player/player.py
+++ music_player/player.py
@@ -15,19 +15,21 @@ album_list_index = 0
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', music_list=jaurim.music_list)
 
 
 @app.route('/play/<song>')
 def play(song):
     global album_list_index
     song_list_index = int(song)
-    target = "자우림 " + str(album_list_index) + "집 " + str(jaurim.music_list[album_list_index-1][song_list_index-1])
+    target = "자우림 " + str(album_list_index) + "집 " +\
+        str(jaurim.music_list[album_list_index-1][song_list_index-1])
     video_list = youtube.search_videos(target)
     video_id = video_list[0]['id']['videoId']
     socketio.emit("response", {
         'type': 'Play',
         'data': video_id,
+        'index': song_list_index,
         },
         namespace='/mynamespace',
         broadcast=True)
@@ -41,11 +43,11 @@ def select(album):
     socketio.emit("response", {
         'type': 'Select',
         'data': album,
+        'index': album_list_index,
         },
         namespace='/mynamespace',
         broadcast=True)
     return 'Get Song'
-
 
 
 @socketio.on('connect', namespace='/mynamespace')
